e12eaa74736c5c091001134789bb5079e437a750
Seongmun-Hong <hsm63746244@gmail.com>
2020-02-12 11:09:43 +0900

    add : 5-2

--- webfront/5장/1/1.js
+++ webfront/5장/1/1.js
@@ -1,9 +1,11 @@
 function Health(name, lastTime) {
-    this.name = name;
-    this.lastTime = lastTime;
-    this.showHealth = function(){
-        console.log(this.name + "님, 오늘은 " + this.lastTime + "에 운동을 하셨네요");      
-    }
+  this.name = name;
+  this.lastTime = lastTime;
+  this.showHealth = function() {
+    console.log(
+      this.name + "님, 오늘은 " + this.lastTime + "에 운동을 하셨네요"
+    );
+  };
 }
 const healthObject = new Health("크롱", "AM 10:12");
 
